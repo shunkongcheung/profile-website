819f8746c8781c1fd93bdb357a48b0a7495073cd
Shun Cheung <scheung@qmo.io>
2021-08-01 08:37:37 -0400

    feat(src): init

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title> Welcome</title>
+      </Head>
+      <div>hello world</div>
+    </>
+  )
+}
