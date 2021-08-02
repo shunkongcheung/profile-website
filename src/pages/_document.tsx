3ea327135b12b75e18d6c9674addd39365b101e9
Shun Cheung <scheung@qmo.io>
2021-08-01 23:25:34 -0400

    feat(_document.tsx): roboto font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,10 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <title>My page</title>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
           {/* Step 5: Output the styles in the head  */}
           {(this.props as any).styleTags}
         </Head>
