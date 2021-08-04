541bc0d0e9801309469fdbe09d7219abce0d3b49
Shun Cheung <scheung@qmo.io>
2021-08-04 13:35:51 -0400

    feat(Landing): rewrite init

--- src/containers/Home/Landing.tsx
+++ src/containers/Home/Landing.tsx
@@ -1,6 +1,5 @@
 import React, { memo } from "react";
-import Image from "next/image";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 import { Carousel } from "../../components";
 import { I18N, Lang } from "../../types";
@@ -8,232 +7,84 @@ import { I18N, Lang } from "../../types";
 import BlendImage from "./BlendImage";
 import FlippyImage from "./FlippyImage";
 import ThreeDImage from "./ThreeDImage";
-import { useWindowSize } from "../../hooks";
 
 interface LandingProps {
   lang: Lang;
-  handleNext: () => any;
 }
 
-const fadeIn = keyframes`
-0% { opacity: 0% };
-100% { opacity: 100% };
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const blink = keyframes`
-0% { opacity: 0% };
-50% { opacity: 100% };
-100% { opacity: 0% };
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-left: auto;
+  margin-right: auto;
+
+  height: 5rem;
+  margin-bottom: 2rem;
 `;
 
-const ArrowDown = styled(Image)<{ delay: number }>`
-  cursor: pointer;
-  animation: ${blink} 2s ease-in-out infinite;
-  animation-delay: ${(props) => props.delay}s;
-  color: white;
+const CarouselContainer = styled.div`
+  width: 100%;
+  height: 500px;
 `;
 
-const LeftColumn = styled.div<{ width: number; height: number }>`
-  flex: 1;
-  display: none;
+const Name = styled.h1`
+  margin: 0;
+  text-align: center;
+  color: ${(props) => props.theme.colors.primary[500]};
 
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
-
-  border-radius: 1rem;
-  overflow: hidden;
-  @media (min-width: 600px) {
-    display: block;
-  }
-`;
-
-const Caption = styled.h3`
-  color: white;
-
-  opacity: 0;
-  animation ${fadeIn} 0.3s linear forwards;
-  animation-delay: 2s;
-  margin: 0 0 0 auto;
-
-  font-size: 1rem;
-  @media (min-width: 600px) {
+  font-size: 1.5rem;
+  @media (min-width: 700px) {
     font-size: 2rem;
   }
 `;
 
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: relative;
-`;
-
-const Content = styled.div`
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  flex-wrap: wrap;
-
-  padding: 1rem;
-  @media (min-width: 600px) {
-    padding: 5rem;
-  }
-`;
-
-const Background = styled.div`
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(
-    ${(props) => props.theme.colors.primary[500]},
-    ${(props) => props.theme.colors.primary[500]},
-    ${(props) => props.theme.colors.primary[600]}
-  );
-  position: absolute;
-
-  z-index: -1;
-`;
-
-const Row = styled.div`
-  display: flex;
-  flex: 1;
-  flex-wrap: wrap;
-
-  margin-top: auto;
-  margin-bottom: auto;
-
-  height: 25rem;
-  max-height: 80%;
-
-  flex-direction: column;
-  @media (min-width: 600px) {
-    flex-direction: row;
-  }
-`;
-
-const RightColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  flex: 1;
-  height: 100%;
-  justify-content: space-between;
-`;
-
-const Title = styled.h1`
-  text-wrap: wrap;
-  letter-spacing: 3px;
-  text-align: end;
-  font-weight: 500;
-  margin: 0 0 0 auto;
-
-  transition: opacity;
-  animation: ${fadeIn} 0.5s ease-in forwards;
-
+const Location = styled.h2`
+  margin: 0;
+  font-weight: 300;
   color: white;
-
-  font-size: 3rem;
-  width: 20rem;
-  @media (min-width: 600px) {
-    font-size: 4rem;
-    width: 30rem;
-  }
-`;
-
-const NextContainer = styled.div`
-  display: flex;
-  margin-top: auto;
-  justify-content: center;
-  width: 100%;
-`;
-
-const NextBtn = styled.button`
-  border: 0;
-  background: transparent;
-  display: flex;
-  flex-direction: column;
 `;
 
 const TRANS: { [x: string]: I18N } = {
-  welcome: {
-    en: "Nice to meet you",
-    zh: "觀迎到來",
+  name: {
+    en: "Shun Kong Cheung",
+    zh: "張淳罡",
   },
-  caption: {
-    en: "Click the arrows",
-    zh: "",
+  location: {
+    en: "Toronto, Canada",
+    zh: "多倫多 加拿大",
   },
 };
 
-const Landing: React.FC<LandingProps> = ({ handleNext, lang = "en" }) => {
+const Landing: React.FC<LandingProps> = ({ lang = "en" }) => {
   const [idx, setIdx] = React.useState(0);
   React.useEffect(() => {
     const clear = setInterval(() => setIdx((o) => (o + 1) % 3), 5000);
     return () => clearInterval(clear);
   }, [setIdx]);
 
-  const windowSize = useWindowSize();
-  const sizes = React.useMemo(() => {
-    if (!windowSize.width) return { width: 0, height: 0 };
-
-    const ratio = 4 / 3;
-
-    const WIDTH_PERC = 0.4;
-
-    const byWidth = {
-      width: windowSize.width * WIDTH_PERC,
-      height: (windowSize.width * WIDTH_PERC) / ratio,
-    };
-
-    if (byWidth.height < windowSize.height * 0.8) return byWidth;
-
-    const HEIGHT_PERC = 0.4;
-    const byHeight = {
-      height: windowSize.height * HEIGHT_PERC,
-      width: windowSize.height * HEIGHT_PERC * ratio,
-    };
-    return byHeight;
-  }, [windowSize]);
-
   return (
     <Container>
-      <Background />
       <Content>
-        <Row>
-          <LeftColumn {...sizes}>
-            <Carousel control={{ idx }}>
-              <ThreeDImage
-                imgSrc={"/home-second-image.jpg"}
-                isStart={idx === 0}
-              />
-              <FlippyImage
-                imgSrc={"/home-first-image.jpg"}
-                isStart={idx === 1}
-              />
-              <BlendImage
-                imgSrc={"/home-third-image.jpg"}
-                isStart={idx === 2}
-              />
-            </Carousel>
-          </LeftColumn>
-          <RightColumn>
-            <Title>{TRANS.welcome[lang]}</Title>
-            <Caption>{TRANS.caption[lang]}</Caption>
-          </RightColumn>
-        </Row>
-        <NextContainer>
-          <NextBtn onClick={handleNext}>
-            {Array.from({ length: 3 }).map((_, idx) => (
-              <ArrowDown
-                src="/arrow-down.svg"
-                height={30}
-                width={30}
-                key={`ArrowDown-${idx}`}
-                delay={idx * 0.3}
-              />
-            ))}
-          </NextBtn>
-        </NextContainer>
+        <Location>{TRANS.location[lang]}</Location>
+        <Name>{TRANS.name[lang]}</Name>
       </Content>
+      <CarouselContainer>
+        <Carousel control={{ idx }}>
+          <ThreeDImage imgSrc={"/home-second-image.jpg"} isStart={idx === 0} />
+          <FlippyImage imgSrc={"/home-first-image.jpg"} isStart={idx === 1} />
+          <BlendImage imgSrc={"/home-third-image.jpg"} isStart={idx === 2} />
+        </Carousel>
+      </CarouselContainer>
     </Container>
   );
 };
