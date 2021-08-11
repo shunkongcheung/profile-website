77b3e1cca56034531e52bba77995c5b42de48b79
shun cheung <shunkong.cheung@gmail.com>
2021-08-10 22:30:04 -0400

    Feature/self description (#17)
    * feat(Landing): add a bio
    
    * feat(AppBar): lang text
    
    Co-authored-by: Shun Cheung <scheung@qmo.io>

--- src/containers/Home/Landing.tsx
+++ src/containers/Home/Landing.tsx
@@ -15,31 +15,43 @@ interface LandingProps {
 
 const Container = styled.div`
   width: 100%;
+
+  margin-bottom: 2rem;
+
   display: flex;
   flex-direction: column;
-  align-items: center;
+
+  @media (min-width: 700px) {
+    flex-direction: row;
+    height: 20rem;
+  }
 `;
 
 const Content = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
 
-  margin-left: auto;
-  margin-right: auto;
-
-  height: 5rem;
-  margin-bottom: 2rem;
+  @media (min-width: 700px) {
+    align-items: start;
+    margin-right: auto;
+    width: 40%;
+  }
 `;
 
 const CarouselContainer = styled.div`
   width: 100%;
   height: 250px;
 
-  @media (min-width: ${(props) => props.theme.breakpoints.sm}px) {
-    height: 500px;
+  @media (min-width: 700px) {
+    width: 50%;
+    height: 300px;
   }
+`;
+
+const Desc = styled.p`
+  line-height: 1.5rem;
+  color: ${(props) => props.theme.colors.primary[300]};
 `;
 
 const MyImage = styled.div<{ src: string }>`
@@ -53,8 +65,7 @@ const MyImage = styled.div<{ src: string }>`
 `;
 
 const Name = styled.h1`
-  margin: 0;
-  text-align: center;
+  margin-top: 0;
   color: ${(props) => props.theme.colors.primary[500]};
 
   font-size: 1.5rem;
@@ -70,6 +81,10 @@ const Location = styled.h2`
 `;
 
 const TRANS: { [x: string]: I18N } = {
+  desc: {
+    en: "Advanced in TypeScript, Python, React.js, Next.js. Proficient in Linux, React Native Firebase, Netlify, Vercel, Verilog and MatLab. Experienced in building web / mobile applications and hosting backend services with AWS. Enthusiastic in becoming a software engineer under a pioneering technological firm.",
+    zh: "精通 TypeScript、Python、React.js、Next.js。 熟用 Linux、React Native Firebase、Netlify、Vercel、Verilog 和 MatLab。 擁有使用 AWS 構建 Web/移動應用程序和託管後端服務的經驗。 熱衷於成為一家領先的技術公司的軟件工程師。 ",
+  },
   name: {
     en: "Shun Kong Cheung",
     zh: "張淳罡",
@@ -93,8 +108,9 @@ const Landing: React.FC<LandingProps> = ({ lang = "en" }) => {
   return (
     <Container>
       <Content>
-        <Location>{TRANS.location[lang]}</Location>
         <Name>{TRANS.name[lang]}</Name>
+        <Location>{TRANS.location[lang]}</Location>
+        <Desc>{TRANS.desc[lang]}</Desc>
       </Content>
       <CarouselContainer>
         <Carousel control={{ idx }}>
