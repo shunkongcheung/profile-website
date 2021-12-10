82decdc2185eb5aaee2d99b7825802cd85c398f0
Shun Kong Cheung <skcheung@amazon.com>
2021-12-09 22:14:39 -0800

    feat(utils): removed

--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import fetchHighlight from "./fetchHighlight";
-import fetchHighlights from "./fetchHighlights";
-import fetchJobs from "./fetchJobs";
-import fetchTools from "./fetchTools";
-
-export { fetchHighlight, fetchHighlights, fetchJobs, fetchTools };
