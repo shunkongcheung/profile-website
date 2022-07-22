79463f3f35572a0df294ff298ccd0ab35fdc5d88
Shun Kong Cheung <shcheung@microsoft.com>
2022-07-21 17:27:27 -0700

    feat(_app): getTheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,49 +1,13 @@
 import React from "react";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { DarkModeContext } from "../hooks/useDarkModeContext";
+import { getTheme } from "../utils";
 
 type Mode = "dark" | "light";
 
 function MyApp({ Component, pageProps }) {
-  const [mode, setMode] = React.useState<Mode>("dark");
-  const darkColors = {
-    background: "#2d3e50",
-    primary: {
-      50: "#fff1f2",
-      100: "#ffe4e6",
-      200: "#fecdd3",
-      300: "#fda4af",
-      400: "#fb7185",
-      500: "#f43f5e",
-      600: "#e11d48",
-      700: "#be123c",
-      800: "#9f1239",
-      900: "#881337",
-    },
-  };
-
-  const lightColors = {
-    background: "white",
-    primary: {
-      50: "#18181b",
-      100: "#27272a",
-      200: "#3f3f46",
-      300: "#52525b",
-      400: "#71717a",
-      500: "#a1a1aa",
-      600: "#d4d4d8",
-      700: "#e4e4d7",
-      800: "#f4f4f5",
-      900: "#fafafa",
-    },
-  };
-
-  const theme = {
-    colors: mode === "dark" ? darkColors : lightColors,
-    breakpoints: {
-      sm: 600,
-    },
-  };
+const [mode, setMode] = React.useState<Mode>("dark");
+  const theme = getTheme(mode)
 
   const GlobalStyle = createGlobalStyle`
 html,
